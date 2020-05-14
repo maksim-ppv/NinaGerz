66397997a6dbcd428ce0b6751739c80acce88cc2
maksim-ppv <popovm1502@gmail.com>
2020-05-15 02:51:44 +0300

    Правки мобильной версии

--- js/main.js
+++ js/main.js
@@ -90,13 +90,13 @@ $(document).ready(function() {
     $("a.navbar__link").click(function () {
     var elementClick = $(this).attr("href")
     var destination = $(elementClick).offset().top;
-    jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 500);
+    jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
     return false;
     });
 });
 
 wow = new WOW({
-  mobile: false,
+  // mobile: false,
   offset: 50
 
 })
